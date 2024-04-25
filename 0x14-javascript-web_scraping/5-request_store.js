d8409efa6beb782572c5332dc41cf2eaa3f45efd
Honeycash <endurancechery@gmail.com>
2024-04-25 09:47:18 -0700

    update

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,11 +1,26 @@
 #!/usr/bin/node
 
-// Import the built-in Node.js 'fs' module
+// write the content of webpage to afile
+
 const fs = require('fs');
-
-// Import the 'request' module
 const request = require('request');
+const siteUrl = process.argv[2];
+const fileSave = process.argv[3];
 
-// Use the 'request' module to perform an HTTP GET request to the URL
-request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
+if (process.argv.length < 4) {
+  console.error('incorrect number of args');
+  process.exit(1);
+}
 
+request(siteUrl, (error, response, body) => {
+  if (error) {
+    console.error('failed');
+    return;
+  }
+  try {
+    const fileContent = body;
+    fs.writeFileSync(fileSave, fileContent);
+  } catch (err) {
+    console.error(err);
+  }
+});
